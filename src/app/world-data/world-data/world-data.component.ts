b3b8f1efc152c5dff3f055079cc605d2a0d357c3
Animesh kant <akant.30@gmail.com>
2020-04-17 05:09:32 +0530

    data added

--- src/app/world-data/world-data/world-data.component.ts
+++ src/app/world-data/world-data/world-data.component.ts
@@ -18,11 +18,13 @@ import { NoCommaPipe } from 'src/app/shared/pipes/no-comma.pipe';
 export class WorldDataComponent implements OnInit {
 
   baseUrl = 'https://corona-virus-stats.herokuapp.com/api/v1/cases/general-stats';
+  countrApi = 'https://api.coronatracker.com/v2/analytics/country';
   totalCnf: any;
   totalAct: any;
   totalRcv: any;
   totalDth: any;
   lastUpdated: any;
+  countryArray: any;
 
   constructor(
     private http: HttpClient,
@@ -33,20 +35,27 @@ export class WorldDataComponent implements OnInit {
 
   ngOnInit() {
     this.getWorldData();
+    this.getCountryData();
   }
 
   getWorldData() {
     let tempData;
     this.http.get(this.baseUrl).subscribe(res => {
-      console.log(res);
       tempData = res;
-      console.log(tempData.data);
       this.totalCnf = tempData.data.total_cases;
       this.totalRcv = tempData.data.recovery_cases;
       this.totalDth = tempData.data.death_cases;
       this.totalAct = tempData.data.currently_infected;
       this.lastUpdated = tempData.data.last_update;
-    })
+    });
+  }
+
+  getCountryData() {
+    this.http.get(this.countrApi).subscribe(response => {
+      const tempCnt = response;
+      this.countryArray = tempCnt;
+    });
+
   }
 
 }
